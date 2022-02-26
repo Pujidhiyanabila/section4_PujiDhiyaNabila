7330c6535afbfbbde0d9fc5051a0e2ab8ce3a894
Pujidhiyanabila <nabilapuji26@gmail.com>
2022-02-26 18:14:31 +0700

    Section 4 fix selesai

--- dart_application_1/bin/luaslingkaran.dart
+++ dart_application_1/bin/luaslingkaran.dart
@@ -1,8 +1,7 @@
 import 'dart:io';
 void main() {
   const phi = 3.14;
-  stdout.write("Masukkan panjang jari-jari lingkaran: ");
-  int r = int.parse(stdin.readLineSync());
+  double r = 14;
   double luas = phi * r * r;
   print("Luas lingkaran adalah $luas");
 }
